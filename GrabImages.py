5fa6bb8671e2f07abd90e3cee0fcf47b44097061
Scott Vars <142948724+CoderScott2023@users.noreply.github.com>
2024-11-19 08:25:57 -0700

    Update GrabImages.py
    So this will essentially grab the NDVI (Normalized Difference Vegetation Index), of each satellite image in a month. We can then loop through each image for each year to see the vegetation, and correlate it with pollution later.
    
    Source: https://ipad.fas.usda.gov/cropexplorer/Definitions/spotveg.htm#:~:text=The%20Normalized%20Difference%20Vegetation%20Index,green%20vegetation%20from%20bare%20soils.

--- GrabImages.py
+++ GrabImages.py
@@ -1,4 +1,6 @@
 import ee
+import cv2
+import numpy
 
 ee.Initialize()
 
@@ -8,3 +10,14 @@ wasatchFrontAreaBounding = ee.Geometry.Polygon([
 ])
 
 images = ee.ImageCollection('LANDSAT/LC08/C02/T1_TOA').filterDate('2000-01-01', '2020-01-01').filterBounds(wasatchFrontAreaBounding).filterMetadata('CLOUD_COVER', 'less_than', 10)
+
+def getMedianImageOfMonth(year, month):
+  startDate = f'{year}-{month:02d}-01'
+  endDate = f'{year}-{month:02d}-30' if month != 2 else f'{year}-{month:02d}-28'
+  monthlyImages = images.filterDate(startDate, endDate).median()
+  return monthlyImages
+
+def createNDVImap(year, month):
+  monthlyImages = getMedianImageOfMonth(year, month)
+  ndvi = monthlyImages.normalizedDifference(["B4", "B5"])
+  return ndvi
