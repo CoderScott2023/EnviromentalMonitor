4c77630d9f983b0c4b336c51f5327ab3cffc447f
Scott Vars <142948724+CoderScott2023@users.noreply.github.com>
2024-11-20 08:34:41 -0700

    Create CreateMaps.py
    Basically, this is rasterio, which can pull data from geotiff files, I used https://rasterio.readthedocs.io/en/latest/topics/reading.html to figure out how. Additionally, it uses a bit of pyplot to visualize which I already knew a bit of. I just have to figure out how to use the directories, because I thought it was supposed to export to my drive

---
+++ CreateMaps.py
@@ -0,0 +1,35 @@
+import os
+import rasterio
+import numpy as np
+import matplotlib.pyplot as plt
+
+dataDir = ''
+outputDir = ''
+os.makedirs(output_dir, exist_ok=True)
+
+def classify_ndvi(ndviData):
+    ndviClass = np.zeros_like(ndviData, dtype=np.uint8)
+    ndviClass[ndviData > 0.6] = 3
+    ndviClass[(ndviData > 0.2) & (ndviData <= 0.6)] = 2
+    ndviClass[ndviData <= 0.2] = 1
+    return ndviClass
+  
+for filename in sorted(os.listdir(dataDir)):
+    if filename.endswith('.tif'):
+        filePath = os.path.join(dataDir, filename)
+        
+        with rasterio.open(filePath) as src:
+            ndviData = src.read(1)
+            
+        ndvi_class = classify_ndvi(ndviData)
+
+        cmap = plt.cm.get_cmap('RdYlGn', 3)  # Red to Green colormap
+        plt.figure(figsize=(10, 8))
+        plt.imshow(ndvi_class, cmap=cmap, interpolation='nearest')
+        plt.colorbar(ticks=[1, 2, 3], label='NDVI Classification')
+        plt.title(f'NDVI Color Map - {filename}')
+        plt.axis('off')
+        
+        output_file = os.path.join(output_dir, f'{filename.replace(".tif", ".png")}')
+        plt.savefig(output_file, dpi=300, bbox_inches='tight')
+        plt.close()
