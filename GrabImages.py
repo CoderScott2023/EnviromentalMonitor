dd7176dcbc85f3c8c42d1272fadf051f4e823bde
Scott Vars <142948724+CoderScott2023@users.noreply.github.com>
2024-12-01 09:14:28 -0700

    Update GrabImages.py
    No cloud cover, less images and there aren't that many images of the Wasatch Front so we need all we can get.

--- GrabImages.py
+++ GrabImages.py
@@ -10,8 +10,7 @@ wasatchFrontAreaBounding = ee.Geometry.Polygon([
 
 images = ee.ImageCollection('LANDSAT/LC08/C02/T1_TOA') \
     .filterDate('2000-01-01', '2020-01-01') \
-    .filterBounds(wasatchFrontAreaBounding) \
-    .filterMetadata('CLOUD_COVER', 'less_than', 10)
+    .filterBounds(wasatchFrontAreaBounding)
 
 def getMedianImageOfMonth(year, month):
     startDate = f'{year}-{month:02d}-01'
