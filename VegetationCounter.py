44b5f21025fea21e5491af09a56dbd9d71f3e949
Scott Vars <142948724+CoderScott2023@users.noreply.github.com>
2024-12-17 15:45:35 -0700

    Update VegetationCounter.py
    This crops the image down to just the map for analyzation

--- VegetationCounter.py
+++ VegetationCounter.py
@@ -1,2 +1,18 @@
 import cv2
 import matplotlib.pyplot as plt
+
+image_path = "NDVI_Year_2001_Month_12.png"
+image = cv2.imread(image_path)
+
+y_start = 85
+y_end = -50
+x_start = 50
+x_end = -350
+
+cropped_image = image[y_start:y_end, x_start:x_end]
+
+plt.imshow(cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB))
+plt.show()
+
+output_path = "Cropped_NDVI_Map.png"
+cv2.imwrite(output_path, cropped_image)
