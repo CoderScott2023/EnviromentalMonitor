2a15b78b917adf89837c54954f4df6c71e55afd4
Scott Vars <142948724+CoderScott2023@users.noreply.github.com>
2024-12-19 10:08:45 -0700

    Update VegetationCounter.py
    This loops through everything and crops them

--- VegetationCounter.py
+++ VegetationCounter.py
@@ -5,18 +5,21 @@ from google.colab import drive
 dataDir = '/content/drive/My Drive/NDVI_Maps'
 outputDir = '/content/drive/My Drive/Cropped_Maps'
 
-image_path = "NDVI_Year_2001_Month_12.png"
-image = cv2.imread(image_path)
+k = 0
 
-y_start = 85
-y_end = -50
-x_start = 50
-x_end = -350
+for x in range(k):
+    image_path = "NDVI_Year_2001_Month_12.png"
+    image = cv2.imread(image_path)
 
-cropped_image = image[y_start:y_end, x_start:x_end]
+    y_start = 85
+    y_end = -50
+    x_start = 50
+    x_end = -350
 
-plt.imshow(cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB))
-plt.show()
+    cropped_image = image[y_start:y_end, x_start:x_end]
+
+    plt.imshow(cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB))
+    plt.show()
 
 output_path = "Cropped_NDVI_Map.png"
 cv2.imwrite(output_path, cropped_image)
