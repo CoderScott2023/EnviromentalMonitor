0574982a21d17c0760076a38cd63314b895beefe
Scott Vars <142948724+CoderScott2023@users.noreply.github.com>
2024-12-20 09:31:25 -0700

    Update VegetationCounter.py
    So this loops through everything in my drive and will crop it for analyzation later.

--- VegetationCounter.py
+++ VegetationCounter.py
@@ -1,25 +1,32 @@
 import cv2
-import matplotlib.pyplot as plt
+import os
+from matplotlib import pyplot as plt
 from google.colab import drive
+
+drive.mount('/content/drive')
 
 dataDir = '/content/drive/My Drive/NDVI_Maps'
 outputDir = '/content/drive/My Drive/Cropped_Maps'
 
-k = 0
+os.makedirs(outputDir, exist_ok=True)
 
-for x in range(k):
-    image_path = "NDVI_Year_2001_Month_12.png"
-    image = cv2.imread(image_path)
+y_start, y_end = 85, -50
+x_start, x_end = 50, -350
 
-    y_start = 85
-    y_end = -50
-    x_start = 50
-    x_end = -350
+for filename in os.listdir(dataDir):
+    if filename.endswith('.png') or filename.endswith('.jpg'):
+        input_path = os.path.join(dataDir, filename)
+        output_path = os.path.join(outputDir, f"Cropped_{filename}")
 
-    cropped_image = image[y_start:y_end, x_start:x_end]
+        image = cv2.imread(input_path)
+        if image is None:
+            continue
 
-    plt.imshow(cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB))
-    plt.show()
+        cropped_image = image[y_start:y_end, x_start:x_end]
 
-output_path = "Cropped_NDVI_Map.png"
-cv2.imwrite(output_path, cropped_image)
+        cv2.imwrite(output_path, cropped_image)
+        print(f"Saved cropped image to {output_path}")
+
+        plt.imshow(cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB))
+        plt.axis('off')
+        plt.show()
