b49c296908346e2727c8c1c5eed93247b5d5c58a
Scott Vars <142948724+CoderScott2023@users.noreply.github.com>
2024-12-31 11:11:58 -0700

    Update ExtrapolateData.py
    This unpacks the file with all of the values to use.

--- ExtrapolateData.py
+++ ExtrapolateData.py
@@ -1,2 +1,14 @@
 import os
 import tensorflow as tf
+
+file_path = "NDVI_Pixel_Counts.txt"
+
+data = []
+with open(file_path, "r") as file:
+    for line in file:
+        line = line.strip()
+        if line.startswith("Month"):
+            current_month = int(line.split(" ")[1].strip(":"))
+        elif line:
+            category, count = line.split(":")
+            data.append((current_month, category.strip(), int(count.strip())))
